7240c6247e4e6426de8d0a3067d07ebe4ad14e02
Mr_Flight233 <mrflight@163.com>
2019-08-29 15:44:32 +0800

    02修改V1.0

--- src/com/test01/HelloWorld.java
+++ src/com/test01/HelloWorld.java
@@ -3,5 +3,6 @@ package com.test01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("02修改");
     }
 }
